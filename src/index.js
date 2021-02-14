9943873de331675cf758a237b56d57232d09faee
Faiz Azmi Rekatama <faizkort@gmail.com>
2021-02-14 18:14:06 +0700

    improvement: handle hydration for prerenderd page (for crawler)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import Router from './router';
 
-render(<Router />, document.getElementById('reboil-root'));
+const rootElement = document.getElementById('reboil-root');
+
+/**
+ * We check if pre-rendering is already happen
+ * to prevent unnecessary rendering
+ */
+if (rootElement.hasChildNodes()) {
+  hydrate(<Router />, rootElement);
+} else {
+  render(<Router />, rootElement);
+}

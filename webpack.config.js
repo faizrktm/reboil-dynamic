1bfe5ee24361ac457a20802a0e1d88b7845306bf
Faiz Azmi Rekatama <faizkort@gmail.com>
2021-03-08 17:24:09 +0700

    refactor: configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,29 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-// const BrotliPlugin = require('brotli-webpack-plugin');
-// const CompressionsPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
+const CompressionsPlugin = require('compression-webpack-plugin');
 const webpack = require('webpack');
 
 const IS_DEV = process.env.NODE_ENV !== 'production';
 
+const APP_PATH = path.resolve(__dirname, 'src/client');
+
 module.exports = {
-  entry: {
-    main: './src/client/index.tsx',
-  },
-  mode: IS_DEV ? 'development' : 'production',
+  entry: APP_PATH,
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
+  mode: IS_DEV ? 'development' : 'production',
   devtool: IS_DEV ? 'inline-source-map' : 'source-map',
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         include: path.join(__dirname, 'src/client'),
         use: 'babel-loader',
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        include: path.join(__dirname, 'src/client'),
       },
     ],
   },
@@ -43,24 +38,24 @@ module.exports = {
     new BundleAnalyzerPlugin({
       analyzerMode: process.env.ANALYZE ? 'server' : 'disabled',
     }),
-    // !IS_DEV &&
-    //   new CompressionsPlugin({
-    //     filename: '[name].gz[query]',
-    //     algorithm: 'gzip',
-    //     test: /\.(js|css|html)$/,
-    //     minRatio: 0.7,
-    //   }),
-    // !IS_DEV &&
-    //   new BrotliPlugin({
-    //     asset: '[path].br[query]',
-    //     test: /\.(js|css|html)$/,
-    //     minRatio: 0.7,
-    //   }),
+    !IS_DEV &&
+      new CompressionsPlugin({
+        filename: '[name].gz',
+        algorithm: 'gzip',
+        test: /\.(js|css|html)$/,
+        minRatio: 0.7,
+      }),
+    !IS_DEV &&
+      new BrotliPlugin({
+        asset: '[file].br',
+        test: /\.(js|css|html)$/,
+        minRatio: 0.7,
+      }),
     IS_DEV && new webpack.HotModuleReplacementPlugin(),
     IS_DEV && new ReactRefreshWebpackPlugin(),
   ].filter(Boolean),
   resolve: {
-    extensions: ['.js', '.jsx', '.tsx', '.ts'],
+    extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
   },
   devServer: {
     hot: true,
